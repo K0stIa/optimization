84eef18ac64f99d52a8590776cd3f03045d82a8e
Kostia <antonjuk@gmail.com>
2015-01-07 14:29:19 +0100

    add berm compile on CMP

--- bmrm/build_bmrm.sh
+++ bmrm/build_bmrm.sh
@@ -2,12 +2,15 @@ rm bmrm.py bmrm.pyc _bmrm.so
 
 swig -c++ -python  bmrm_solver.i
 
+//CFLAGS=$(shell python-config --cflags)
+//LDFLAGS=$(shell python-config --ldflags)
+
 gcc -fPIC -c libqp_splx.cpp bmrm_solver.cpp bmrm_solver_wrap.cxx \
   -I/usr/include/python2.7 \
   -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python/numpy/core/include \
   -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python
 
-g++ -shared libqp_splx.o  bmrm_solver.o bmrm_solver_wrap.o -o _bmrm.so
+g++ -shared -lpython2.7 libqp_splx.o  bmrm_solver.o bmrm_solver_wrap.o -o _bmrm.so
 
 rm *.o
 rm bmrm_solver_wrap.*
