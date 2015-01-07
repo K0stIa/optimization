f867c1de4b73f0c738ef4173f28f0faa648c4b7f
Kostia <antonjuk@gmail.com>
2015-01-07 13:45:12 +0100

    update berm_install scrip for mac os

--- bmrm/build_bmrm.sh
+++ bmrm/build_bmrm.sh
@@ -3,11 +3,11 @@ rm bmrm.py bmrm.pyc _bmrm.so
 swig -c++ -python  bmrm_solver.i
 
 gcc -fPIC -c libqp_splx.cpp bmrm_solver.cpp bmrm_solver_wrap.cxx \
-    -I/usr/include/python2.7 \
-    -I/usr/lib/python2.7/site-packages/numpy/core/include \
-    -I/usr/lib/python2.7
+  -I/usr/include/python2.7 \
+  -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python/numpy/core/include \
+  -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python
 
-g++ -shared  libqp_splx.o  bmrm_solver.o bmrm_solver_wrap.o -o _bmrm.so
+g++ -shared libqp_splx.o  bmrm_solver.o bmrm_solver_wrap.o -o _bmrm.so
 
 rm *.o
-# rm bmrm_solver_wrap.cxx bmrm_solver_wrap.h
+rm bmrm_solver_wrap.*
