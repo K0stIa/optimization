81bbe5734d5e67da7fb1bd1677ed34636dcf1a49
Kostia <antonjuk@gmail.com>
2015-03-05 11:16:27 +0100

    update building bmrm scripts

--- bmrm/build_bmrm.sh
+++ bmrm/build_bmrm.sh
@@ -2,15 +2,20 @@ rm bmrm.py bmrm.pyc _bmrm.so
 
 swig -c++ -python  bmrm_solver.i
 
-//CFLAGS=$(shell python-config --cflags)
-//LDFLAGS=$(shell python-config --ldflags)
+#CFLAGS=$(shell python-config --cflags)
+#LDFLAGS=$(shell python-config --ldflags)
 
 gcc -fPIC -c libqp_splx.cpp bmrm_solver.cpp bmrm_solver_wrap.cxx \
-  -I/usr/include/python2.7 \
-  -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python/numpy/core/include \
-  -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python
+  -I/usr/local/Cellar/python/2.7.9/Frameworks/Python.framework/Versions/2.7/include/python2.7 \
+  -I/usr/local/lib/python2.7/site-packages/numpy/core/include \
+  -I/usr/local/Cellar/python/2.7.9/Frameworks/Python.framework/Versions/2.7/lib/python2.7
 
-g++ -shared -lpython2.7 libqp_splx.o  bmrm_solver.o bmrm_solver_wrap.o -o _bmrm.so
+# -I/usr/include/python2.7 
+#  -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python/numpy/core/include \
+#  -I/System/Library/Frameworks/Python.framework/Versions/2.7/Extras/lib/python
+
+# -lpython2.7
+g++ -shared -L/usr/local/Cellar/python/2.7.9/Frameworks/Python.framework/Versions/2.7/lib -lpython2.7  libqp_splx.o  bmrm_solver.o bmrm_solver_wrap.o -o _bmrm.so
 
 rm *.o
 rm bmrm_solver_wrap.*
